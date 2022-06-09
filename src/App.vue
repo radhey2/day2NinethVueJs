833ef2bae1253e8274dd49e3711c94049557fd25
lenovo <radheshyamb9@gmail.com>
2022-06-09 11:24:37 +0530

    ADDED : V-ONCE and
     V-PRE
    Not quite as useful, v-once will not update once it's been rendered.

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+ <div id="app">
+  <h3>What is your favorite kind of taco?</h3>
+  <p><input v-model="tacos" /></p>
+  <p v-once="tacos">{{ tacos }}</p>
+  <span v-pre>This is good if I need to show the mustache view of {{ tacos }}</span>
+  <pre>{{ $data }}</pre>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+   data() {
+    return {
+      tacos: 'I like Al Pastor tacos'
+    }
+  }
+}
+</script>
